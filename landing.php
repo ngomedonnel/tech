94ed10edf005592421043dfc0767652f33463a77
techTrai <allinchat@gmail.com>
2024-04-27 08:19:33 +0100

    have added bootstrap, bg and remove all js code

--- landing.php
+++ landing.php
@@ -6,10 +6,16 @@
   <title>techchallenges</title>
   <link rel="stylesheet" href="landing.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+
 </head>
 <body>
-  <div class="container">
-    <div id="title"><h1  class="tit">WELCOME TO TECH TRIALBLAZERS</h1></div>  <hr>
+  <div class="container1">
+    <div id="title"><h1  class="tit">WELCOME TO TECH TRIALBLAZERS</h1></div>
     <div id="P"><p>
       For all those who are ready to join our coding challenge, your are welcome to get started by <br>clicking the button below
     </p></div>
@@ -55,89 +61,75 @@
     <div class="heading">
       <h1>INTERNScam coming soon...</h1>
     </div>
-  <div class="slideshow-container">
+  
+    
+    <div id="carouselExampleSlidesOnly1" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
 
-<!-- Full-width images with number and caption text -->
-   <div class="mySlides fade">
-    <div class="numbertext">1 / 5</div>
-    <img src="images/home.png" style="width:100%">
-   </div>
-
-   <div class="mySlides fade">  
-    <div class="numbertext">2 / 5</div>
-    <img src="images/jobmanagement dashboard.png" style="width:100%">
-   </div>
-
-   <div class="mySlides fade">
-    <div class="numbertext">3 / 5</div>
-    <img src="images/jobs listing.png" style="width:100%">
-  </div>
-
-   <div class="mySlides fade">
-     <div class="numbertext">4 / 5</div>
-      <img src="images/Screenshot 2024-04-07 095220.png" style="width:100%">
-</div>
-
-   <div class="mySlides fade">
-  <div class="numbertext">5 / 5</div>
-  <img src="images/welcome.png" style="width:100%">
-</div>
-
-<!-- The dots/circles -->
-    <div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-  <span class="dot" onclick="currentSlide(4)"></span>
-  <span class="dot" onclick="currentSlide(5)"></span>
-
+        <div class="carousel-item active" >
+          <img src="./images/welcome.png" class="d-block w-80" alt="...">
+        </div>
+        <div class="carousel-item ">
+          <img src="./images/home.png" class="d-block w-80" alt="...">
+        </div>
+        <div class="carousel-item ">
+          <img src="./images/jobs listing.png" class="d-block w-80" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/jobmanagement dashboard.png" class="d-block w-80" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="./images/profile.png" class="d-block w-80" alt="...">
+          </div>
+      </div>
     </div>
 
-  </div>
-  <div class="shortnote">
-    Get ready for one of  TECH TRAILBLAZERS  upcoming internship web app <br> where student can easily get internship in cameroon just by applying .
-  </div>
+    <div class="shortnote">
+     Get ready for one of  TECH TRAILBLAZERS  upcoming internship web app <br> where student can easily get internship in cameroon just by applying .
+    </div>
   </div>  
+
+
   <!-- container for the third carousel -->
   <div class="container3">
     <div class="heading">
       <h1>CAMERMARKETPLACE coming soon...</h1>
     </div>
-    <div class="slideshow-container2">
-      <div class="mySlides2 fade2">
-        <div class="numbertext">1 / 3</div>
-        <img src="images/Screenshot 2024-03-15 115132.png" style="width:100%">
-      </div>
-      
-      <div class="mySlides2 fade2">  
-          <div class="numbertext">2 / 3</div>
-          <img src="images/Screenshot 2024-03-15 131131.png" style="width:100%">
-      </div>
-      
-      <div class="mySlides2 fade2">
-          <div class="numbertext">3 / 3</div>
-          <img src="images/Screenshot 2024-03-15 115227.png" style="width:100%">
-      </div>
-    </div> 
-      
-      <!-- The dots/circles -->
-        <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-        <!-- <span class="dot" onclick="currentSlide(4)"></span>
-        <span class="dot" onclick="currentSlide(5)"></span> -->
-      
-      </div>
-      <div class="shortnote">
-        Imagine creating your store online rather than paying or developing ur own website  <br>
-        now that is  CAMERMARKETPLACE an online market where user can easily buy and sell products
-       </div>
-       
+
+    <div id="carouselExampleSlidesOnly2" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+
+          <div class="carousel-item active" >
+            <img src="./images/registration.png" class="d-block w-80" alt="...">
+          </div>
+          <div class="carousel-item ">
+            <img src="./images/login.png" class="d-block w-80" alt="...">
+          </div>
+          <div class="carousel-item ">
+            <img src="./images/market.png" class="d-block w-80" alt="...">
+          </div>
+          
+        </div>
     </div>
    
+    <div class="shortnote">
+      Imagine creating your store online rather than paying or developing ur own website  <br>
+      now that is  CAMERMARKETPLACE an online market where user can easily buy and sell products
+    </div>
+
+  </div>
+   
+
+
     
-  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script>
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+</script>
+
 <script src="index.js"></script>
 </body>
 </html>
